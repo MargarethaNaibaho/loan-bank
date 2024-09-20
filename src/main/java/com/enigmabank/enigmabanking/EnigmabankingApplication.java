5ee9cd828b25c9de87c8993abe8b7f531b0c1bf5
Margaretha Naibaho <margarethanaibaho2@gmail.com>
2024-09-20 16:49:06 +0700

    Initial Commit

---
+++ src/main/java/com/enigmabank/enigmabanking/EnigmabankingApplication.java
@@ -0,0 +1,13 @@
+package com.enigmabank.enigmabanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnigmabankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnigmabankingApplication.class, args);
+	}
+
+}
